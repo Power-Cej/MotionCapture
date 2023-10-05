8e8c1b83eb127b90091a4bb80e3f77a32f0629e0
ceejay900 <58793044+ceejay900@users.noreply.github.com>
2023-10-05 16:51:43 +0800

    Motion Capture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+    } catch (err) {
+      console.error('Error accessing the camera:', err);
+    }
+  };
+
+  useEffect(() => {
+    startCamera()
+  }, [])
+
+  const takePicture = () => {
+    const context = canvasRef.current.getContext('2d');
+    context.drawImage(videoRef.current, 0, 0, 640, 480);
+    const imageData = canvasRef.current.toDataURL('image/jpeg');
+
+    fetch('http://localhost:5000/myServer', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ imageData }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.error('Error uploading picture:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Camera</h1>
+      <div className="line"></div>
+      <video ref={videoRef} width="640" height="480" autoPlay />
+      <div>
+        {/* <button onClick={startCamera}>Start Camera</button> */}
+        <button onClick={takePicture}>Take Picture</button>
+      </div>
+      <canvas ref={canvasRef} style={{ display: 'none' }} width="640" height="480" />
     </div>
   );
-}
+};
 
 export default App;
